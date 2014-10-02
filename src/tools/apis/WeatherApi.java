f9e71f3625ed8ea50f50f97e88c42cffd65eda53
Marwan Ghanem <marwan.t.ghanem@gmail.com>
2014-10-02 10:31:56 +0200

    Current Weather/Weather Forecast

---
+++ src/tools/apis/WeatherApi.java
@@ -0,0 +1,67 @@
+package tools.apis;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+public class WeatherApi {
+		
+	private static String readAll(BufferedReader rd) throws IOException {
+	    StringBuilder sb = new StringBuilder();
+	    int cp;
+	    while ((cp = rd.read()) != -1) {
+	      sb.append((char) cp);
+	    }
+	    return sb.toString();
+	  }
+	
+	public static JSONObject getWeatherNow(double x , double y){
+		try {
+			String address = "http://api.openweathermap.org/data/2.5/weather?lat="+x+"&lon="+y+"";
+			System.out.println(address);
+		 
+			URL url = new URL(address);		 
+			BufferedReader in = new BufferedReader(new InputStreamReader(
+					url.openStream()));
+			String str = readAll(in);
+			in.close();
+			JSONObject json = new JSONObject(str);		
+			return json;
+		} catch (JSONException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	
+	public static JSONObject getWeatherForeCast(double x , double y , int cnt){
+		try {
+		String address = "http://api.openweathermap.org/data/2.5/forecast/daily?lat="+x+"&lon="+y+"&cnt="+cnt+"&mode=json";
+		System.out.println(address);
+		 
+		URL url = new URL(address);		 
+		BufferedReader in = new BufferedReader(new InputStreamReader(
+				url.openStream()));
+		String str = readAll(in);
+		in.close();
+		JSONObject json = new JSONObject(str);		
+		return json;
+		} catch (JSONException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	public static void main(String args[]){
+		System.out.println(getWeatherNow(48.43,2.25));
+		System.out.println(getWeatherForeCast(48.43,2.25,10));
+	}
+	
+}
